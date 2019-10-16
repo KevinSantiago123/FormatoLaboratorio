8d2a9a81d08bc39c565b4a6e750282a64eb0f16a
Kevin Castañeda <kevin.castaneda@servinformacion.com>
2019-10-16 13:21:08 -0500

    avance formulario v2

--- src/app/formulario-laboratorio/formulario-laboratorio.component.ts
+++ src/app/formulario-laboratorio/formulario-laboratorio.component.ts
@@ -13,6 +13,7 @@ import { FormularioLaboratoriosService,FormularioAsignaturasService} from '../se
 export class FormularioLaboratorioComponent {
   myLaboratorio = new FormControl();
   myAsignatura = new FormControl();
+  myControl = new FormControl();
   asignatura: Asignaturas[];
   laborato: Laboratorios[];
 
@@ -22,10 +23,36 @@ export class FormularioLaboratorioComponent {
   labelPosition = 'after';
   disabled = false;
 
+  checked1 = false;
+  indeterminate1 = false;
+  labelPosition1 = 'after';
+  disabled1 = false;
+
+  checked2 = false;
+  indeterminate2 = false;
+  labelPosition2 = 'after';
+  disabled2 = false;
+
+  checked3 = false;
+  indeterminate3 = false;
+  labelPosition3 = 'after';
+  disabled3 = false;
+
+  checked4 = false;
+  indeterminate4 = false;
+  labelPosition4 = 'after';
+  disabled4 = false;
+
+  checked5 = false;
+  indeterminate5 = false;
+  labelPosition5 = 'after';
+  disabled5 = false;
+
   constructor( 
-    public listadoLabService: FormularioLaboratoriosService, public listadoAsigService: FormularioAsignaturasService  
+    private listadoLabService: FormularioLaboratoriosService, private listadoAsigService:FormularioAsignaturasService
     ) { 
     this.laborato = this.listadoLabService.getLaboratorios();
     this.asignatura = this.listadoAsigService.getAsignaturas();
+
     }
 }
